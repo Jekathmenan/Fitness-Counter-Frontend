f1ff845f0942b5e3b833a09f1b2918b82f0fb233
Jekath <>
2026-07-31 01:09:07 +0200

    Add Template for Login View

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -31,8 +31,22 @@
 
 </script>
 <template>
-    <div>
+    <div class="login-container">
         
+        <form class="login-form" @submit.prevent="handleLogin">
+            <h2 class="title">Anmelden</h2> 
+            <div class="form-field">
+                
+                <label class="form-label" for="email">E-Mail</label>
+                <input name="email" v-model="email" type="email" placeholder="E-Mail" required />
+            </div>
+            <div class="form-field">
+                <label class="form-label" for="password">Passwort</label>
+                <input name="password" v-model="password" type="password" placeholder="Passwort" required />
+            </div>
+            <button class="login-submit" type="submit">Login</button>
+        </form>
+        <p v-if="errorMessage" class="error"> {{ errorMessage }}</p>
     </div>
 </template>
 <style lang="css" scoped>
