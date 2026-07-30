e546129972edac63dab406574cbd62ec04abf6ae
Jekath <>
2026-07-31 01:09:25 +0200

    Add Styling to LoginView

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -50,4 +50,84 @@
     </div>
 </template>
 <style lang="css" scoped>
+    .title {
+        margin-bottom: 5px;
+        font-size: 28px !important;
+    }
+
+    .login-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 50vh;
+        margin: 60px 25px;
+        
+    }
+
+    .login-form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 70px;
+        border-radius: 25px;
+        border: 2px solid black; 
+        box-shadow: 0 4px 20px rgba(0,0,0,0.3);
+        background-color: #F9F9F2; 
+    }
+
+    .form-field {
+        margin: 5px 18px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .form-label {
+        font-size: 15px !important;
+        width: 100%;
+        margin-bottom: 2px;
+    }
+
+    .form-field input {
+        font-size: 15px !important;
+        border-radius: 8px;
+        padding: 5px;
+        width: 500px;
+        text-align: center;
+        border: 1px solid #2c3e50;
+    }
+
+    .login-submit {
+        margin-top: 8px;
+        width: 500px;
+        border-radius: 25px;
+        background-color: #2c3e50;
+        padding: 5px;
+        color: white;
+        border: 1px solid white;
+    }
+
+    .login-submit:hover {
+        margin-top: 8px;
+        width: 500px;
+        border-radius: 25px;
+        background-color: beige ;
+        padding: 5px;
+        border: 1px solid #2c3e50;
+        color: #2c3e50;
+    }
+
+    .error { 
+        color: red; margin-top: 10px; 
+        width: 400px;
+        padding: 4px 120px;
+        text-align: center;
+        border-radius: 25px;
+        
+        background-color: rgb(233, 116, 116);
+    }
+
 </style>
