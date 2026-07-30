0d715201b623f70150624775d820ee99dbc595aa
Jekath <>
2026-07-31 01:04:30 +0200

    Add authStore
    Add Token as state
    Add isAuthenticated as State
    Add login and logout-Actions to centralize these functions

---
+++ src/stores/auth.js
@@ -0,0 +1,32 @@
+import apiClient from '@/api/client';
+import { defineStore } from 'pinia';
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        token: localStorage.getItem('token') || null,
+    }),
+    getters: {
+        isAuthenticated: (state) => !!state.token,
+    },
+    actions: {
+        async login(credentials) {
+            try {
+                const response = await apiClient.post('auth/login', credentials);
+
+                const token = response.data.token;
+
+                // Token im State und localStorage speichern
+                this.token = token;
+                localStorage.setItem('token', token);
+                return response.data;
+            } catch (error) {
+                console.error("Login-Fehler im Store: ", error);
+                throw error;
+            }
+        },
+        logout() {
+            this.token = null;
+            localStorage.removeItem('token');
+        }
+    }
+});
