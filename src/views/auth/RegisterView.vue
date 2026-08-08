dd9dd76f6f07ee5f8ac29e91fb24307bc99fc88f
Jekath <>
2026-08-08 16:25:38 +0200

    Implementing Showpassword Button

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -13,6 +13,9 @@
         retypePassword: ''
     });
 
+    const showPassword = ref(false);
+    const showRetypePassword = ref(false);
+
     const errors = ref({});
 
     const validateInputs = () =>  {
@@ -144,18 +147,34 @@
                     <label class="block text-sm font-medium text-white mb-1" for="password">
                         Passwort
                     </label>
-                    <input name="password" v-model="form.password" 
-                    class="w-full px-4 py-3 rounded-lg border border-gray-300 
-                        focus:ring-2 focus:ring-blue-500 focus:border-transparent text-white outline-none transition"
-                        type="password" placeholder="Passwort"/>
+                    <div class="relative">
+                        <input name="password" v-model="form.password" 
+                            class="w-full px-4 py-3 rounded-lg border border-gray-300  bg-gray-700 text-white
+                            focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition pr-20"
+                            :type="showPassword ? 'text' : 'password'" placeholder="Passwort"/>
+                        <button type="submit" @click="showPassword = !showPassword" class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-white transition">
+                            {{ showPassword ? 'Verbergen' : 'Anzeigen' }}
+                        </button>
+                    </div>
+                    
                 </div>
                 <div class="form-group">
                     <label class="block text-sm font-medium text-white mb-1" for="retypePassword">
                             Passwort wiederholen
                     </label>
-                    <input name="retypePassword" v-model="form.retypePassword" 
-                        class="w-full px-4 py-3 rounded-lg border border-gray-300 
-                        focus:ring-2 focus:ring-blue-500 focus:border-transparent text-white outline-none transition" type="password" placeholder="Passwort wiederholen"/>
+                    <div class="relative">
+                        <input name="retypePassword" v-model="form.retypePassword" 
+                            class="w-full px-4 py-3 rounded-lg border border-gray-300 
+                            focus:ring-2 focus:ring-blue-500 focus:border-transparent text-white outline-none transition pr-20" :type="showRetypePassword ? 'text' : 'password'" placeholder="Passwort wiederholen"/>
+                        <button
+                        type="button"
+                        @click="showRetypePassword = !showRetypePassword"
+                        class="absolute inset-y-0 right-0 pr-4 flex items-center text-xs font-bold uppercase tracking-widest text-gray-400 hover:text-blue-400 transition"
+                        >
+                        {{ showRetypePassword ? 'Verbergen' : 'Anzeigen' }}
+                        </button>
+                    </div>
+                    
                     <span v-if="errors.password" class="mt-2 text-sm text-red-600">
                         {{ errors.password }}
                     </span>
