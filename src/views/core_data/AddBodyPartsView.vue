7651127890bc095d10f1fe10a4c70a534078bd40
Jekath <>
2026-08-12 18:34:50 +0200

    Add page "AddBodyParts"

---
+++ src/views/core_data/AddBodyPartsView.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import Header from '@/components/template/Header.vue';
+</script>
+
+<template>
+  <Header title="Neues Körperteil erfassen" 
+    path="/core-data/body-parts" 
+    urlText="Zur&uuml;ck zur &Uuml;bersicht" 
+  />
+</template>
