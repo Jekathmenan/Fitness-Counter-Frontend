c74e0e4fa44e42ba3de474f01c0d8b49efabf006
Jekath <>
2026-08-12 18:47:59 +0200

    Fix OverviewCard imports

--- src/views/core_data/Overview.vue
+++ src/views/core_data/Overview.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import OverviewCard from '@/components/CoreDate/OverviewCard.vue';
+    import OverviewCard from '@/components/template/CoreDate/OverviewCard.vue';
     
 </script>
 <template>
