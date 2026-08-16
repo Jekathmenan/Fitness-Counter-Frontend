b24ee62d53c06908f08ccaaee7582a7ba16417c9
Jekath <>
2026-08-16 12:01:15 +0200

    Create ExerciseOverview page with standard template

--- src/views/core_data/ExercisesView.vue
+++ src/views/core_data/ExercisesView.vue
@@ -1,6 +1,20 @@
 <script setup>
+    import Header from '@/components/template/Header.vue';
+    import { ref, onMounted } from 'vue';
+    import { useFlashStore } from '@/stores/flash';
+    import apiClient from '@/api/client';
+    import { Pencil, Trash2 } from 'icons';
+
+    const flashStore = useFlashStore();
+    const exercises = ref({});
+    
 </script>
 <template>
+    <Header title="Übungen verwalten"
+        path="/core-data"
+        urlText="Zur&uuml;ck zu den Stammdaten"
+        addText="Hinzuf&uuml;gen"
+        addPath="/core-data/exercises/add"
 
 </template>
 <style lang="css" scoped>
