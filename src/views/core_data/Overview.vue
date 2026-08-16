5fa9aa0a11379761684fab4053295c8e74d1ff33
Jekath <>
2026-08-16 11:58:56 +0200

    Add "ExercisesOverview" card in coredata-overview page

--- src/views/core_data/Overview.vue
+++ src/views/core_data/Overview.vue
@@ -5,7 +5,8 @@
 <template>
     <!-- TODO: Implement a Breadcrump -->
     <div class="flex justify-items-center items-center w-full">
-        <OverviewCard title="Körperteile" path="/core-data/body-parts" urlText="Zu Körperteileverwaltung" />
+        <OverviewCard title="K&ouml;rperteile" path="/core-data/body-parts" urlText="Zu Körperteileverwaltung" />
+        <OverviewCard title="&Uuml;bungen" path="/core-data/exercises" urlText="Zu &Uuml;bungsverwaltung" />
     </div>
     
 </template>
