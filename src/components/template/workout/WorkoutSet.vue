b0803140b05acd18e6c7ce6b62a2d95d6b73e376
Jekath <>
2026-09-04 14:58:35 +0200

    Implementing WorkoutSet-Component
    Similar to the WorkoutExercise component, the WorkoutSet manages all actions regarding a single workoutSet. If changes happen the parent is notified via emit

---
+++ src/components/template/workout/WorkoutSet.vue
@@ -0,0 +1,110 @@
+<script setup>
+    import { ref } from 'vue';
+    import { onMounted } from 'vue';
+    import {  Check , Trash2 } from 'icons';
+    import { useFlashStore } from '@/stores/flash';
+    import apiClient from '@/api/client';
+
+    const props = defineProps({
+        set: {
+            type: Object,
+            required: true
+        },
+        exerciseId: {
+            type: Number,
+            required: true
+        }
+        
+    });
+
+    const emit = defineEmits(["setChanged",]);
+
+    const flash = useFlashStore();
+    const weight = ref(0);
+    const reps = ref(0);
+    const errors = ref({});
+
+    const saveChanges = async (set) => {
+        try {
+            // validate Input
+            const weightVal = parseFloat(weight.value);
+            const repsVal = parseFloat(reps.value);
+
+            await validateInput(weightVal, repsVal);
+
+            if (!errors.value.weight && !errors.value.weight) {
+                const body = {
+                    id: props.set.id,
+                    weight: weightVal,
+                    reps: repsVal
+                };
+
+                const response = await apiClient.put("workout/"+ props.exerciseId + "/set", body);
+                emit("setChanged", response.data);
+                flash.setFlash("Satz gespeichert", "success");
+            }
+
+        } catch (error) {
+            flash.setFlash("Fehler beim speichern des Satzes", "error");
+            console.error(error);
+        }
+    };
+
+    const validateInput = async (weightVal, repsVal) => {
+        errors.value.weight = false;
+        errors.value.reps = false;
+
+        if (isNaN(weightVal) || weightVal < 0) {
+            errors.value.weight = true;
+        }
+
+        if (isNaN(repsVal) || repsVal < 0) {
+            errors.value.reps = true;
+        }
+
+        return errors.value.weight || errors.value.weight;
+    };
+
+    const openDeleteModal = () => {
+
+    };
+
+    const handleDelete = async () => {
+
+    };
+
+    onMounted(() => {
+        weight.value = props.set.weight;
+        reps.value = props.set.reps;
+    });
+</script>
+<template>
+    <!--<td class="px-3 py-2 text-gray-500 font-mono text-xs">
+        {{ set.pos }}
+    </td> -->
+    <td class="px-3 py-2 font-bold text-white hover:text-blue-400 transition-colors">
+        <input v-model="weight" maxlength="4" 
+            :class="errors.weight ? 'border-red-500 text-red-500' : 'border-gray-600 focus:border-blue-500 text-white'"    
+            class="w-15 px-2 text-center rounded-lg border border-gray-600" />
+
+    </td>
+    <td class="px-3 py-2 font-bold text-white hover:text-blue-400 transition-colors">
+        <input v-model="reps" maxlength="4" class="w-15 px-2 text-center rounded-lg border border-gray-600" />
+        <span v-if="errors.reps" 
+            class="mt-2 text-sm text-red-600"
+            >
+            {{ errors.reps }}
+        </span>
+    </td>
+    
+    <td class="px-3 py-2">
+        <div class="flex justify-end space-x-2">
+            <button @click="saveChanges" class="p-2 rounded-lg bg-gray-700/50 text-blue-400 hover:bg-blue-500 hover:text-white transition-all shadow-sm" title="Bearbeiten">
+                <Check  class="w-4 h-4" />
+            </button>
+            <button  @click="openDeleteModal" class="p-2 rounded-lg bg-gray-700/50 text-red-400 hover:bg-red-500 hover:text-white transition-all shadow-sm" title="Löschen">
+                <Trash2 class="w-4 h-4" />
+            </button>
+        </div>
+    </td>
+</template>
